74f4aed884c219ebc92a97c2e33662a3b48f565c
jamesh2075 <jhenry2075@msn.com>
2023-10-17 15:26:26 -0400

    Fixed LINT errors in vlan.model.spec.ts

--- src/app/shared/simulation/emulator/cisco/model/vlan.model.spec.ts
+++ src/app/shared/simulation/emulator/cisco/model/vlan.model.spec.ts
@@ -1,33 +1,33 @@
-import {Vlan} from "./vlan.model";
+import {Vlan} from './vlan.model';
 export function main() {
 
     describe('new Vlan()', () => {
-        let vlan = new Vlan();
+        const vlan = new Vlan();
 
         it('vlan diff should be empty', () => {
-            expect(vlan.serialize(true)).toEqual({$class:'Vlan'});
+            expect(vlan.serialize(true)).toEqual({$class: 'Vlan'});
         });
     });
 
     describe('assigned an ID on new Vlan()', () => {
-        let vlan = new Vlan();
+        const vlan = new Vlan();
         vlan.id = 10;
 
         it('vlan diff should include ID', () => {
-            expect(vlan.serialize(true)).toEqual({$class:'Vlan', id:10, name: "VLAN0010"});
+            expect(vlan.serialize(true)).toEqual({$class: 'Vlan', id: 10, name: 'VLAN0010'});
         });
     });
 
     describe('assigned an defaultValues on new Vlan()', () => {
-        let vlan = new Vlan();
+        const vlan = new Vlan();
         vlan.setDefaultValues({
-            status: "inactive"
+            status: 'inactive'
         });
         vlan.status = 'active';
 
         it('vlan diff should include $defaultValues', () => {
             expect(vlan.serialize(true)).toEqual({
-                $class:'Vlan',
+                $class: 'Vlan',
                 status: 'active',
                 $defaultValues: {
                     status: 'inactive'
