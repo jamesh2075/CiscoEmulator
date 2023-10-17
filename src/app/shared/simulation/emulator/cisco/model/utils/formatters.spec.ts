c0eab42e94809e58147306fe863e2215e711f944
jamesh2075 <jhenry2075@msn.com>
2023-10-17 15:10:53 -0400

    Fixed LINT errors in formatters.spec.ts

--- src/app/shared/simulation/emulator/cisco/model/utils/formatters.spec.ts
+++ src/app/shared/simulation/emulator/cisco/model/utils/formatters.spec.ts
@@ -1,60 +1,60 @@
-import {toJSONObject} from "./formatters";
+import {toJSONObject} from './formatters';
 export function main() {
 
     describe('formatters()', () => {
 
         describe('toJSONObject({id: 1})', () => {
             it('expect to be {id:1}', () => {
-                let json = toJSONObject({id: 1});
+                const json = toJSONObject({id: 1});
                 expect(json).toEqual({id: 1});
             });
         });
 
         describe('toJSONObject(1)', () => {
             it('expect to be null', () => {
-                let json = toJSONObject(1);
+                const json = toJSONObject(1);
                 expect(json).toBe(1);
             });
         });
 
         describe('toJSONObject(true)', () => {
             it('expect to be null', () => {
-                let json = toJSONObject(true);
+                const json = toJSONObject(true);
                 expect(json).toBe(true);
             });
         });
 
         describe('toJSONObject("")', () => {
             it('expect to be ""', () => {
-                let json = toJSONObject("");
-                expect(json).toBe("");
+                const json = toJSONObject('');
+                expect(json).toBe('');
             });
         });
 
         describe('toJSONObject(null)', () => {
             it('expect to be null', () => {
-                let json = toJSONObject(null);
+                const json = toJSONObject(null);
                 expect(json).toBe(null);
             });
         });
 
         describe('toJSONObject(undefined)', () => {
             it('expect to be undefined', () => {
-                let json = toJSONObject(undefined);
+                const json = toJSONObject(undefined);
                 expect(json).toBe(undefined);
             });
         });
 
         describe('toJSONObject({_snapshot:true, id: 1})', () => {
             it('expect to be {id:1}', () => {
-                let json = toJSONObject({_snapshot: true, id: 1});
+                const json = toJSONObject({_snapshot: true, id: 1});
                 expect(json).toEqual({_snapshot: true, id: 1});
             });
         });
 
         describe('toJSONObject({_snapshot:true, id: 1})', () => {
             it('expect to be {id:1}', () => {
-                let json = toJSONObject({_snapshot: true, id: 1}, true);
+                const json = toJSONObject({_snapshot: true, id: 1}, true);
                 expect(json).toEqual({id: 1});
             });
         });
