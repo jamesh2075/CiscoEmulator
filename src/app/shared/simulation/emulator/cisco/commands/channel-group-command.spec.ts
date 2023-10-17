d0e0befcfa88e98c409e07e9c1664d70b2172d11
jamesh2075 <jhenry2075@msn.com>
2023-10-17 17:33:28 -0400

    Fixed LINT issues in command-tester

--- src/app/shared/simulation/emulator/cisco/commands/channel-group-command.spec.ts
+++ src/app/shared/simulation/emulator/cisco/commands/channel-group-command.spec.ts
@@ -6,7 +6,7 @@ import { CiscoCommandParser } from '../command-parser';
 import { CiscoCommandContext } from '../cisco-terminal-command';
 import { channelGroupCommand } from './channel-group-command';
 import { NotSupportedCommand } from './notsupported';
-import { CommandConstants } from "../common/cisco-constants";
+import { CommandConstants } from '../common/cisco-constants';
 import { CommandTester, CommandTestCase, CommandTestCaseResult } from './command-tester';
 import { noChannelGroupModeCommand, ChannelGroupCommand } from './channel-group-command';
 
@@ -15,20 +15,20 @@ export function main() {
     const testCases: CommandTestCase[] = [{
         // minimum value correct
         commands: 'channel-group   1    mode active',
-        model: { 'channelGroup': { id: 1, mode: ChannelGroupCommand.STRINGS.CHANNEL_GROUP_MODE_ACTIVE, 
+        model: { 'channelGroup': { id: 1, mode: ChannelGroupCommand.STRINGS.CHANNEL_GROUP_MODE_ACTIVE,
                                     protocol: ChannelGroupCommand.STRINGS.CHANNEL_PROTOCOL_LACP } }
     }, {
         commands: 'channel-group 2 mode desirable',
-        model: { 'channelGroup': { id: 2, mode: ChannelGroupCommand.STRINGS.CHANNEL_GROUP_MODE_DESIRABLE, 
+        model: { 'channelGroup': { id: 2, mode: ChannelGroupCommand.STRINGS.CHANNEL_GROUP_MODE_DESIRABLE,
                                     protocol: ChannelGroupCommand.STRINGS.CHANNEL_PROTOCOL_PAgP } }
     }, {
         commands: 'channel-group 2 mode auto',
-        model: { 'channelGroup': { id: 2, mode: ChannelGroupCommand.STRINGS.CHANNEL_GROUP_MODE_AUTO, 
+        model: { 'channelGroup': { id: 2, mode: ChannelGroupCommand.STRINGS.CHANNEL_GROUP_MODE_AUTO,
                                     protocol: ChannelGroupCommand.STRINGS.CHANNEL_PROTOCOL_PAgP } }
     }, {
         // maximum value correct
         commands: 'channel-group 255 mode active',
-        model: { 'channelGroup': { id: 255, mode: ChannelGroupCommand.STRINGS.CHANNEL_GROUP_MODE_ACTIVE, 
+        model: { 'channelGroup': { id: 255, mode: ChannelGroupCommand.STRINGS.CHANNEL_GROUP_MODE_ACTIVE,
                                     protocol: ChannelGroupCommand.STRINGS.CHANNEL_PROTOCOL_LACP } }
     },
     {
@@ -38,7 +38,7 @@ export function main() {
             'channel-group 2 mode desireable'
         ],
         output: CommandConstants.ERROR_MESSAGES.INVALID_INPUT
-    },];
+    }];
 
     // Written to handle no channel-group test cases
     // TODO: Write test which are passing
@@ -63,7 +63,7 @@ export function main() {
         // incomplete command
         commands: 'channel-group 0 mode',
         output: CommandConstants.ERROR_MESSAGES.INCOMPLETE_COMMAND
-    },];
+    }];
 
     xdescribe('[channel-group] (under development, known to fail)', () => {
         CommandTester.RunTestCases(excludedCases, channelGroupCommand);
