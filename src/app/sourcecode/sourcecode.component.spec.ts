54b30200926d643856262fc493959492aebb1b15
jamesh2075 <jhenry2075@msn.com>
2023-10-16 21:11:18 -0400

    Added SourceCode component

---
+++ src/app/sourcecode/sourcecode.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SourcecodeComponent } from './sourcecode.component';
+
+describe('SourcecodeComponent', () => {
+  let component: SourcecodeComponent;
+  let fixture: ComponentFixture<SourcecodeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SourcecodeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SourcecodeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
